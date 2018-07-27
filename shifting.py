58dde7ab4c4e7e44e1c30d45451cbd5fc65e7c10
TheFailFatty <41774065+TheFailFatty@users.noreply.github.com>
2018-07-27 20:56:35 +0530

    Add files via upload

--- shifting.py
+++ shifting.py
@@ -8,24 +8,29 @@ word=word.lower()
 word_char=list(word)
 encno=['54','83','27','43','81','18','0','17','23','29','37','72','9','1','3','5','6','8','7','10','50','40','31','35','63','25','99']
 len_word_list=len(word_char)
-encmsg=''
+#encmsg=' '
 len_alp_list=len(alphas)
-#nolist=[]
+#encno2=encno
 encno2=encno
 
-def encode_message(nolist): 
-	for j in range(0,len_word_list):
-		index=alphas.index(word_char[j])
-		encmsg+=nolist[index]+' '
-	print(encmsg)
+def shift_no_list(list_temp):
+    shift_value=random.randint(1,10)
+  #  print(shift_value)
+    list_temp=deque(list_temp)
+    list_temp.rotate(shift_value)
+ #   print(list_temp)
+    list_temp=list(list_temp)
+#    print encno2
+    return list_temp
 
-encno2=encno
+encno2=shift_no_list(encno2)
 
-def shift_no_list():
-	shift_value=random.randint(0,10)
-	encno2=deque(encno2)
-	encno2.rotate(shift_value)
-	encno2=list(encno2)
+#print "TEST:- ",encno2
 
-shift_no_list()
-encode_message(encno2)
+def encode_message(encmsg):
+    for j in range(0,len_word_list):
+        index=alphas.index(word_char[j])
+        encmsg+=encno2[index]+' '
+    print(encmsg)
+
+encode_message('')
