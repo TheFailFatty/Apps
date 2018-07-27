2122cbcb8fd3acf7ca5042ab344b43c34976ac24
TheFailFatty <41774065+TheFailFatty@users.noreply.github.com>
2018-07-27 21:23:51 +0530

    Add files via upload

--- shifting.py
+++ shifting.py
@@ -8,26 +8,20 @@ word=word.lower()
 word_char=list(word)
 encno=['54','83','27','43','81','18','0','17','23','29','37','72','9','1','3','5','6','8','7','10','50','40','31','35','63','25','99']
 len_word_list=len(word_char)
-#encmsg=' '
 len_alp_list=len(alphas)
-#encno2=encno
 encno2=encno
 
 def shift_no_list(list_temp):
     shift_value=random.randint(1,10)
-  #  print(shift_value)
     list_temp=deque(list_temp)
     list_temp.rotate(shift_value)
- #   print(list_temp)
     list_temp=list(list_temp)
-#    print encno2
-    return list_temp
+    return list_temp,shift_value
 
-encno2=shift_no_list(encno2)
-
-#print "TEST:- ",encno2
+encno2,s_value=shift_no_list(encno2)
 
 def encode_message(encmsg):
+    encmsg=str(s_value)+' '
     for j in range(0,len_word_list):
         index=alphas.index(word_char[j])
         encmsg+=encno2[index]+' '
